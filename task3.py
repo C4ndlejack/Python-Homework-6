331f0cf3ef3d9449a86e70d20398af7a618c3c4e
Candlejack <garouspeak@gmail.com>
2022-10-17 19:43:32 +0300

    task 3 is done

---
+++ task3.py
@@ -0,0 +1,10 @@
+def thesaurus(*args):
+    names_dict = {}
+    for i in sorted(args):
+        letter = i[0]
+        if letter not in names_dict:
+            names_dict[letter] = [i]
+    names_dict[letter] += [i]
+    return names_dict
+
+print(thesaurus("Иван", "Мария", "Пётр", "Илья", "Марина", "Алина", "Бибочка"))
